58cf96688dba19a5e689cba7018b58d0ba03ff15
JohnTD796 <156860528+JohnTD796@users.noreply.github.com>
2024-04-18 18:01:10 -0400

    fixed bug with license not showing properly, added license badge

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,8 @@
 // TODO: Include packages needed for this application
-const { generateKey } = require('crypto');
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
-const fileName = 'log.txt';
+const fileName = '../README.md';
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -55,7 +54,15 @@ const questions = [
     },
 ];
 
-inquirer
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err =>
+        err ? console.log(err) : console.log("Success!"))
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
     .prompt(questions)
 
     .then((data) => {
@@ -63,17 +70,7 @@ inquirer
         writeToFile(fileName, generatedData);
     })
     ;
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err =>
-        err ? console.log(err) : console.log("Success!"))
-    // confused on what this function means (fileName, response)
 }
-
-
-// TODO: Create a function to initialize app
-function init() { }
 
 // Function call to initialize app
 init();
